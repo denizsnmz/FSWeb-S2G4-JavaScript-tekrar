8183e1a5d9646d6fa9e48fff329552137b93b182
denizsnmz <dnzsnmz122@gmail.com>
2024-11-22 02:30:20 +0300

    görev 3 tamamlandı

--- index.js
+++ index.js
@@ -101,7 +101,17 @@ let ucetambolunenler,
 
 // 3a çözümü
 
-/* kodlar buraya */
+let enKucuk = sayilar[0];
+let enBuyuk = sayilar[0];
+
+for (let i = 1; i < sayilar.length; i++) {
+  if (sayilar[i] < enkucuk) {
+    enkucuk = sayilar[i];
+  }
+  if (sayilar[i] > enbuyuk) {
+    enbuyuk = sayilar[i];
+  }
+}
 
 // 3b çözümü:
 
